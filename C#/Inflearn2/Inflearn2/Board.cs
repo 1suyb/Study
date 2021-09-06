f3360b67440de9f16ab7e3543199e7f132eae78a
1suyb <suyb1234@gmail.com>
2021-09-07 01:35:50 +0900

    Lay a Map

---
+++ C#/Inflearn2/Inflearn2/Board.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inflearn2
+{
+    class Board
+    {
+         
+        public void Initialize()
+        {
+
+        }
+    }
+}
